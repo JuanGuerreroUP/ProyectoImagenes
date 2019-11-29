06daed1452759d7c87b7f4143a9c5308ddc36231
Juan Guerrero <0211637@up.edu.mx>
2019-11-29 16:50:52 -0600

    loop with while

--- OjosRojos/OjosRojos/OjosRojos.py
+++ OjosRojos/OjosRojos/OjosRojos.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
+import random
 from circleDetector import Circle
+
 
 #functions
 def gamma(img, gamma):
@@ -9,28 +11,30 @@ def gamma(img, gamma):
 		for i in np.arange(0, 256)]).astype("uint8")
     return cv2.LUT(img, table)
 
+
+def circunferencia(x,h,k,r):
+    return int(pow(pow(r,2)-pow(x-h,2),0.5)+k)
 #end of functions
 
 circulo = Circle();
-circulo.p1 = [5,8]
-circulo.p2 = [10,12]
-circulo.p3 = [3,15]
+circulo.p1 = [-0.5,9]
+circulo.p2 = [2.8,12]
+circulo.p3 = [7,11]
 
-circulo.getEq
-#
-img = cv2.imread("red-eye0.jpg")
 
-tamanio = np.shape(img)
+circulo.getEq()
+print("Centro:",circulo.radius)
+
+img = cv2.imread("red-eye2.jpg")
+#img = cv2.resize(img, )
+tamanio = np.shape(img) #[y,x]
 print("y: ",tamanio[0])
 print("x: ",tamanio[1])
-#img = cv2.resize(img, None, fx=0.5, fy=0.5,interpolation=cv2.INTER_CUBIC)
+
 #BGR
 imgChannels = [img[:,:,0], img[:,:,1], img[:,:,2]]
-#cv2.imwrite("save.jpg",imgChannels[0])
 
-#for i in range(3):
-#    imgChannels[i] = cv2.GaussianBlur(imgChannels[i],(3,3),2)
-#imgFinal = cv2.merge(imgChannels)
+
 imgChannels[2] = gamma(imgChannels[2],0.8) #se aplica función gamma en el canal R (de BGR)
 
 imgMerged = cv2.merge(imgChannels) #se concatenan los tres canales con gamma en canal R
@@ -46,38 +50,106 @@ cv2.imshow("gamma",imgMerged)
 cv2.imshow("gamma-gauss",imgGauss)
 imgHSV = cv2.cvtColor(imgGauss, cv2.COLOR_BGR2HSV)
 print(np.max(imgHSV[:,:,2]))
-#imgReds = np.zeros((tamanio[0],tamanio[1]))
 
-#imgReds = (imgHSV[:,:,0]<= 5) & (imgHSV[:,:,1] > 80) & (imgHSV[:,:,2] > 60)
 print("H max:", np.max(imgHSV[:,:,0]))
 imgReds = (imgHSV[:,:,0] <= 5) & (imgHSV[:,:,1] > 80) & (imgHSV[:,:,2] > 60)
 imgPinks = (imgHSV[:,:,0] >= 120) & (imgHSV[:,:,1] > 80) & (imgHSV[:,:,2] > 60)
 imgReds = imgReds + imgPinks
-imgReds = imgReds.astype(np.uint8)*255
+
+
+
 
 itera = 0
-posibles = []
-for y in range(0,tamanio[0],5):
-    for x in range(0,tamanio[1],5):
-        if(imgReds[y,x]==255):
-            posibles.append([x,y])#append solo cuando se cumpla el algortimo por tangentes para detectar circulos
-#for x in imgReds:
-   # for y in x:
-       # if(y == 255):
-         #   imgReds[x,y]=200
+imgEdges = cv2.Canny(imgReds.astype(np.uint8)*255, 100,100);#edge detection, cambiar a detector de sobel
+imgEdgesCenters = np.zeros((tamanio[0],tamanio[1]),int).astype(np.uint8)
+justCircles = np.zeros((tamanio[0],tamanio[1]),int).astype(np.uint8)
+cv2.imwrite("save.jpg",imgReds.astype(np.uint8)*255)
 
-    
-#imgReds = cv2.resize(imgReds, None, fx=0.5, fy=0.5,interpolation=cv2.INTER_CUBIC)
-cv2.Canny(imgReds, 100,100) #edge detection, cambiar a detector de sobel
 
+tempCirc = Circle()
+x = 0
+#for x in range(0,tamanio[1]):
+ #   for y in range(0, tamanio[0]):
+
+while x < tamanio[1]:
+    #break;
+    y = 0
+    while y < tamanio[0]:
+        found = False
+        if(imgEdges[y,x] == 255):
+            tempCirc.p1 = [x,y]
+            for x in range(tempCirc.p1[0]+1, tamanio[1]):
+                if not(imgReds[y,x]):
+                    tempCirc.p2 = [x,y]
+                    found = True
+                    break;
+                found = False
+            x = int((tempCirc.p1[0]+tempCirc.p2[0])/2)
+            for y in range(tempCirc.p1[1]+1, tamanio[0]):
+                if not(imgReds[y,x]):
+                    tempCirc.p3 = [x,y]
+                    found = True
+                    break;
+                found = False
+            if(found):
+                tempCirc.getEq()
+                imgEdgesCenters[tempCirc.center[1],tempCirc.center[0]] = 200
+
+                successCount = 0
+                tempRadius = int(tempCirc.radius*0.75)
+                for i in range(20):
+                    testX= random.randint(int(tempCirc.center[0] - tempRadius), int(tempCirc.center[0] + tempRadius)) #genera un numero aleatorio dentro del dominio de un circulo con la 3/4 del radio original
+                    testY = circunferencia(testX, tempCirc.center[0], tempCirc.center[1], tempRadius)
+
+                    #en caso de que se encuentre el posible circulo en un borde
+                    if(testX < 0): testX = 0
+                    if(testX >= tamanio[1]): testX = tamanio[1]-1
+                    if(testY < 0): testY = 0
+                    if(testY >= tamanio[0]): testY = tamanio[0]-1
+
+                    if(imgReds[testY,testX]):
+                        successCount +=1
+                    if(imgReds[int(testY-tempRadius),testX]):
+                        successCount +=1
+
+                tempRadius = int(tempCirc.radius*1.25)
+                for i in range(20):
+                    testX= random.randint(int(tempCirc.center[0] - tempRadius), int(tempCirc.center[0] + tempRadius)) #genera un numero aleatorio dentro del dominio de un circulo con la 3/4 del radio original
+                    testY = circunferencia(testX, tempCirc.center[0], tempCirc.center[1], tempRadius)
+
+                    #en caso de que se encuentre el posible circulo en un borde
+                    if(testX < 0): testX = 0
+                    if(testX >= tamanio[1]): testX = tamanio[1]-1
+                    if(testY < 0): testY = 0
+                    if(testY >= tamanio[0]): testY = tamanio[0]-1
+
+                    if not(imgReds[testY,testX]):
+                        successCount +=1
+                    if not(imgReds[int(testY-tempRadius),testX]):
+                        successCount +=1
+
+                if(successCount > 63):
+                    justCircles[int(tempCirc.center[1] - tempRadius):int(tempCirc.center[1] + tempRadius),int(tempCirc.center[0] - tempRadius):int(tempCirc.center[0] + tempRadius)] = 255
+
+                
+        y +=1
+    x +=1
+
+
+
+cv2.imshow("circles", justCircles)
+
+imgReds = imgReds.astype(np.uint8)*255 #convierte a imagen de 8bits
 
 
 #cv2.namedWindow("reds", cv2.WINDOW_NORMAL)  
-cv2.imshow("reds", cv2.Canny(imgReds,100,100))
-#mask = cv2.inRange(imgHSV, lowerRed, upperRed)
-#cv2.imshow("mask",mask)
+cv2.imshow("redEdges", imgEdges)
 
+
+#cv2.imwrite("save.jpg",imgChannels[0])
 cv2.waitKey(0)
+
+
 
 #x^2+y^1+cx+dy+e=0
 #https://www.youtube.com/watch?v=J0526-mTn7g
--- OjosRojos/OjosRojos/circleDetector.py
+++ OjosRojos/OjosRojos/circleDetector.py
@@ -8,37 +8,38 @@ class Circle:
     c,d,e = 0,0,0
     center = []
     radius = 0
-    def getDs():
-        self.ds = np.linalg.det([[mat[0,0],mat[1,0],mat[2,0]],
-                                 [mat[0,1],mat[1,1],mat[2,1]],
-                                 [mat[0,2],mat[1,2],mat[2,0]]])
-    def getDc():
-        self.dc = np.linalg.det([[mat[3,0],mat[1,0],mat[2,0]],
-                                 [mat[3,1],mat[1,1],mat[2,1]],
-                                 [mat[3,2],mat[1,2],mat[2,0]]])
-    def getDd():
-        self.dd = np.linalg.det([[mat[0,0],mat[3,0],mat[2,0]],
-                                 [mat[0,1],mat[3,1],mat[2,1]],
-                                 [mat[0,2],mat[3,2],mat[2,0]]])
-    def getDe():
-        self.de = np.linalg.det([[mat[0,0],mat[1,0],mat[3,0]],
-                                 [mat[0,1],mat[1,1],mat[3,1]],
-                                 [mat[0,2],mat[1,2],mat[3,0]]])
-    def ti(x,y):
+    def getDs(self):
+        self.ds = np.linalg.det([ [self.mat[0][0],self.mat[0][1],self.mat[0][2]],
+                                  [self.mat[1][0],self.mat[1][1],self.mat[1][2]],
+                                  [self.mat[2][0],self.mat[2][1],self.mat[2][2]] ])
+    def getDc(self):
+        #print(self.mat)
+        self.dc = np.linalg.det([ [self.mat[0][3],self.mat[0][1],self.mat[0][2]],
+                                  [self.mat[1][3],self.mat[1][1],self.mat[1][2]],
+                                  [self.mat[2][3],self.mat[2][1],self.mat[2][2]] ])
+    def getDd(self):
+        self.dd = np.linalg.det([ [self.mat[0][0],self.mat[0][3],self.mat[0][2]],
+                                  [self.mat[1][0],self.mat[1][3],self.mat[1][2]],
+                                  [self.mat[2][0],self.mat[2][3],self.mat[2][2]] ])
+    def getDe(self):
+        self.de = np.linalg.det([ [self.mat[0][0],self.mat[0][1],self.mat[0][3]],
+                                  [self.mat[1][0],self.mat[1][1],self.mat[1][3]],
+                                  [self.mat[2][0],self.mat[2][1],self.mat[2][3]] ])
+    def ti(self,x,y):
         return (pow(x,2)+pow(y,2))*-1
-    def createMat():
-        self.mat = [[p1,1,ti(p1[0],p1[1])],[p2,1,ti(p2[0],p2[1])],[p3,ti(p3[0],p3[1])]]
-    def calcCenter(z):
-        return pow(z,2)/(z*(z/2))
-    def getEq():
-        createMat()
-        getDs()
-        getDc()
-        getDd()
-        getDe()
-        self.c = self.dc/self.ds
-        self.d = self.dd/self.ds
+    def createMat(self):
+        self.mat = [[self.p1[0],self.p1[1],1,self.ti(self.p1[0],self.p1[1])],[self.p2[0], self.p2[1],1,self.ti(self.p2[0],self.p2[1])],[self.p3[0], self.p3[1], 1,self.ti(self.p3[0],self.p3[1])]]
+    def calcCenter(self,z):
+        return pow(z/2,2)
+    def getEq(self):
+        self.createMat()
+        self.getDs()
+        self.getDc()
+        self.getDd()
+        self.getDe()
+        self.c = self.dc/(2*self.ds)
+        self.d = self.dd/(2*self.ds)
         self.e = self.de/self.ds
-        self.center = [calcCenter(self.c),calcCenter(self.d)]
-        self.radius = (-1*self.e)+(self.c/2)+(self.d/2)
+        self.center = [int(self.c*-1),int(self.d*-1)]
+        self.radius = int(pow((-1*self.e)+pow(self.c,2)+pow(self.d,2),0.5))
 #    def setCircle(_p1, _p2, _p3):#[x,y] input
